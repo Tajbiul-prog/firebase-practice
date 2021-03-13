426ce0d278b76e7ae6950a063caa7b596c2a7504
Tajbiul-prog <taj04biul@gmail.com>
2021-03-13 23:33:07 +0600

    first commit

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+        apiKey: "AIzaSyBFq-nwqUvRGsKKL1D_nSqcsXclL2M1yKQ",
+        authDomain: "fir-practice-e2691.firebaseapp.com",
+        projectId: "fir-practice-e2691",
+        storageBucket: "fir-practice-e2691.appspot.com",
+        messagingSenderId: "1091413845212",
+        appId: "1:1091413845212:web:fff7647805703067474019"
+      };
+
+      export default firebaseConfig
